b3a753e921ff013fafb0964ae72e4540c6fbf847
The Evisu project <47283629+TheEvisu@users.noreply.github.com>
2023-10-25 16:15:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Slider from "./Slider";
+import ImageFilter from "./ImageFilter";
+
+const tmpImg = {
+  urlOriginal:
+    "https://eventodev-cdn-c3dfhva7grfmffcx.z01.azurefd.net/images/originals/Large/6744012e-9a40-484b-9cec-6a13c5494ec8.webp",
+  urlProcessed:
+    "https://eventodev-cdn-c3dfhva7grfmffcx.z01.azurefd.net/images/edited/Large/6744012e-9a40-484b-9cec-6a13c5494ec8.webp",
+};
 
 function App() {
+  const [intensity, setIntensity] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Snapify</h1>
       </header>
+      <div className="slider-container">
+        <Slider
+          min={0}
+          max={100}
+          value={intensity}
+          handleChange={(e) => setIntensity(e.target.value)}
+        />
+        <p className="slider-value">{intensity}%</p>
+      </div>
+      <ImageFilter
+        intensity={intensity}
+        imgOriginal={tmpImg.urlOriginal}
+        imgProcessed={tmpImg.urlProcessed}
+      />
     </div>
   );
 }
